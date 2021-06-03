a57ea12980b5ec15e3d3a77d82bb15c526869b37
Dhrumil Mehta <dhrumil.m@somaiya.edu>
2021-06-03 18:27:46 -0400

    Sample fullstack DAPP

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.3",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/2a56efe7d9ea4c43909251cf7a79ad12",
+      accounts: [`0x87890bf923e77ab151dbc5af9185118abd1414cd9884dc63ecde4f889e164fa5`]
+    }
+  }
+};
+
